3074b98a40645822f84b3bf41bf3a7f4642b7897
Antonio Torres <antoniotorr5555@gmail.com>
2020-02-20 20:04:49 -0600

    Avance shell, quedan algunos errores

--- shell.c
+++ shell.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
 
 void separar (char *input, char **argv);
-void ejecutar (char **argv);
+void ejecutar (char input[1024]);
 int isPipe(char **argv);
-void ejecutaSinPipe(char **argv);
 void separaPipe(char **argv,char **beforePipe, char **afterPipe);
 void ejecutaConPipe(char **argv, char **beforePipe, char **afterPipe);
 
@@ -20,12 +20,54 @@ void main () {
         fgets(input, sizeof(input), stdin);
         char *p = strchr(input, '\n');
         if (p)  *p = '\0'; //Cambia el salto de línea del final de la cadena por un caracter nulo
+        ejecutar(input);
+    }
+}
+
+void ejecutar (char input[1024]) {
+    char *argv[64];
+    pid_t pid1, pid2;
+    pid1 = fork();
+    if (pid1) {
+        //padre
+        int estado = 0;
+        wait(&estado);
+        if ((estado >> 8) == 24) {
+            exit(1);
+        }
+    }
+    else {
+        //hijo
         separar(input, argv);
-        if (strcmp(*argv, "salir")) {
-            ejecutar(argv);
+        if (!strcmp(*argv, "salir")) {
+            exit(24); // For Kobe
+        }
+        if (isPipe(argv)) {
+            char *beforePipe[64], *afterPipe[64];
+            int pipefd[2];
+            separaPipe(argv, beforePipe, afterPipe);
+            pipe(pipefd);
+            pid2 = fork();
+            if (pid2) {
+                //hijo
+                close(0);
+                close(pipefd[1]);
+                dup(pipefd[0]);
+                execvp(*afterPipe, afterPipe);
+                printf("Error, comando '%s' no encontrado \n", *afterPipe);
+            }
+            else {
+                //nieto
+                close(1); // 1 es la salida estándar
+                close(pipefd[0]);
+                dup(pipefd[1]); // La salida estándar va a ser pipefd[1]
+                execvp(*beforePipe, beforePipe);
+                printf("Error, comando '%s' no encontrado \n", *beforePipe);
+            }
         }
         else {
-            exit(0);
+            execvp(*argv, argv);
+            printf("Error, comando '%s' no encontrado \n", *argv);
         }
     }
 }
@@ -34,69 +76,22 @@ void main () {
 void separar (char *input, char **argv) {
     while (*input != '\0') {
         while (*input == ' '){ // Revisa si hay espacios o tabulaciones
-            *input = '\0'; // Camia espacios por caracteres nulos
+            *input = '\0'; // Cambia espacios por caracteres nulos
             input++; // Avanza al siguiente elemento
         }
         *argv = input; // Guarda la ubicación de la palabra
         argv++; // Avanza al siguiente elemento
         while (*input != '\0' && *input != ' ') { //Si no hay espacios, tabuladores, o caracteres vacíos (insertados por los espacios)
+            if (*input == '|') { // Caso en el que hay un pipe pero no hay espacios antes y después del pipe
+                *argv = input; // Se guarda la posición del pipe
+                argv++;
+                input++;
+                break; //Se sigue a revisar la siguiente palabra
+            }
             input++; //Avanza al siguiente caracter de la palabra
         }
     }
     *argv = (char *) 0; //El último elemento es un carácter nulo
-}
-
-//Revisar el caso para exit
-void ejecutar (char **argv) {
-    if (isPipe(argv)) {
-        char *beforePipe[64];
-        char *afterPipe[64];
-        ejecutaConPipe(argv, beforePipe, afterPipe);
-    }
-    else {
-        ejecutaSinPipe(argv);
-    }
-}
-
-void ejecutaConPipe(char **argv, char **beforePipe, char **afterPipe) {
-    /*
-        Esto no sirve Si quieres arreglalo o haz otra cosa, cambia todo lo que quieras, porque creo que en unas partes mi lógica está dlv
-    separaPipe(argv, beforePipe, afterPipe);
-    int pid, pid2, pipefd[2];
-    pipe(pipefd);
-    pid = fork();
-    if (pid) {
-        pid2 = fork();
-        if (!pid2) { //Revisar por qué no funciona esta parte
-            close(1); // 1 es la salida estándar
-            close(pipefd[0]);
-            dup(pipefd[1]); // La salida estándar va a ser pipefd[1]
-            execvp(*beforePipe, beforePipe);
-        }
-        else {
-            close(0);
-            close(pipefd[1]);
-            dup(pipefd[0]);
-            execvp(*afterPipe, afterPipe);
-        }
-    }
-    else {
-        exit(1);
-    }*/
-}
-
-void ejecutaSinPipe(char **argv) {
-    int pid;
-    int estado;
-    pid = fork();
-    if (pid) {
-        wait(&estado);
-    }
-    else {
-        execvp(*argv, argv);
-        printf("Error, comando: %s no encontrado \n", *argv);
-        exit(0);
-    }
 }
 
 void separaPipe(char **argv,char **beforePipe, char **afterPipe) {
@@ -112,15 +107,22 @@ void separaPipe(char **argv,char **beforePipe, char **afterPipe) {
             }
             else {
                 *beforePipe = *argv;
+                char *p = strchr(*beforePipe, '|');
+                if (p){
+                    *p = '\0';
+                    flag = 1;
+                    *argv++;
+                }
                 *beforePipe++;
             }
         }
         *argv++;
     }
+    *beforePipe = (char *) 0;
+    *afterPipe = (char *) 0;
 }
 
 //Revisa si hay pipe en la instrucción
-//Cambiar esto, a lo mejor ya no se necesita
 int isPipe(char **argv) {
     while (*argv != (char *) 0) {
         if (**argv == '|') {
